2ff793049bdcb430c9080846ace8326b0ce2dbe6
Satish Vishwakarma <satishv96@gmail.com>
2025-12-10 22:55:17 +0000

    Cors error resolve

--- src/main/java/com/cfg/testRxGPT/endpoint/AppointmentsController.java
+++ src/main/java/com/cfg/testRxGPT/endpoint/AppointmentsController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.cfg.testRxGPT.repo.AppointmentsRepo;
 
 
 @RestController
+@CrossOrigin(origins = "http://34.28.54.211:80")
 public class AppointmentsController {
 
 	private static final Logger logger = LogManager.getLogger(AppointmentsController.class);
--- src/main/java/com/cfg/testRxGPT/endpoint/UserController.java
+++ src/main/java/com/cfg/testRxGPT/endpoint/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @RestController
+@CrossOrigin(origins = "http://34.28.54.211:80")
 public class UserController {
 
 	private static final Logger logger = LogManager.getLogger(UserController.class);
